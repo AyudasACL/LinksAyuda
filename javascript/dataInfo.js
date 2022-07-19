fc0e0407f729706e9eb786cf0ee305d41dbda3b2
JavierBagatoli <javi.bagatoli@gmail.com>
2022-07-19 11:22:56 -0300

    feat: codigo js para crear la lista

---
+++ javascript/dataInfo.js
@@ -0,0 +1,27 @@
+// Creada por Joan Salas 14/07
+// Posible formato para los nuevos links agregados
+let objetoLink = {
+  nombre: "Pixabay",
+  descripcion: "Imágenes gratis",
+  link: "https://pixabay.com",
+  //Puede ser categoria o tipo
+  categorias: ["front", "diseño", "webapps"],
+};
+
+let objetoLink2 = {
+  nombre: "Ejemplo",
+  descripcion: "Imágenes gratis",
+  link: "https://pixabay.com",
+  //Puede ser categoria o tipo
+  categorias: ["front", "diseño", "webapps"],
+};
+
+let objetoLink3 = {
+  nombre: "Ejemplo",
+  descripcion: "Imágenes gratis",
+  link: "https://pixabay.com",
+  //Puede ser categoria o tipo
+  categorias: ["front", "diseño", "webapps"],
+};
+
+export const datos = [objetoLink, objetoLink2, objetoLink3];
--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,21 @@
-// Creada por Joan Salas 14/07
-// Posible formato para los nuevos links agregados
-let objetoLink = {
-    nombre: "Pixabay",
-    descripcion: "Imágenes gratis",
-    link: "https://pixabay.com",
-    //Puede ser categoria o tipo
-    categorias: ["front", "diseño", "webapps"],
+//Codigo creado por Javier Bagatoli 16/02/2022
+
+import { datos } from "./dataInfo.js";
+
+const dataBase = datos;
+
+const boton = document.getElementById("caminar");
+boton.innerHTML = generarLista();
+
+function generarLista() {
+  const lista = dataBase;
+  let listaCompletada = "";
+  for (let pagina in dataBase) {
+    listaCompletada += `
+    <article class="articulo">
+      <h3 class="text-white" title="${dataBase[pagina].descripcion}">${dataBase[pagina].nombre}</h3>
+      <a href=${dataBase[pagina].link}>Canva</a>
+    </article>`;
+  }
+  return listaCompletada;
 }
