8e4fc4ee80b86c991794e7733c95357d1b2bd9fc
JavierBagatoli <javi.bagatoli@gmail.com>
2022-07-23 01:51:39 -0300

    feat: Enlaces refactorizados para que esten forma de objetos

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,20 @@
 //Codigo creado por Javier Bagatoli 16/02/2022
 
-import { datos } from "./dataInfo.js";
+import { datos } from "./docs/dataInfo.js";
 
 const dataBase = datos;
 
-const boton = document.getElementById("caminar");
+const boton = document.getElementById("lista");
 boton.innerHTML = generarLista();
 
 function generarLista() {
   const lista = dataBase;
-  let listaCompletada = "";
+  let listaCompletada = "<br/>";
   for (let pagina in dataBase) {
     listaCompletada += `
     <article class="articulo">
       <h3 class="text-white" title="${dataBase[pagina].descripcion}">${dataBase[pagina].nombre}</h3>
-      <a href=${dataBase[pagina].link}>Canva</a>
+      <a href=${dataBase[pagina].link}>${dataBase[pagina].linkNombre}Canva</a>
     </article>`;
   }
   return listaCompletada;
